8d161a29e7a47a6971111fb8153d772a28ff9ddb
Aliaksandr_Kruhlik <Aliaksanadr_Kruhlik@epam.com>
2023-06-13 19:14:12 +0400

    current version

--- PCF/DateRangePicker/DateRangePickerComponent.tsx
+++ PCF/DateRangePicker/DateRangePickerComponent.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
-import { DatePicker, mergeStyleSets, DayOfWeek, IDatePicker, IRefObject } from '@fluentui/react';
+import { DatePicker, mergeStyleSets, DayOfWeek, IDatePicker, IRefObject, IDatePickerStyles } from '@fluentui/react';
 
 interface DateRangePickerProps {
-  onStartDateChange?: (startDate: Date | undefined) => void;
-  onEndDateChange?: (endDate: Date | undefined) => void;
+  onStartDateChange?: (startDate: Date | null | undefined) => void;
+  onEndDateChange?: (endDate: Date | null | undefined) => void;
+  //notifyFn?: () => void;
   containerWidth?: number;
   containerHeight?: number;
   minDate?: Date;
   maxDate?: Date;
+  startDate?: Date;
+  endDate?: Date;
 }
 
 const DateRangePickerComponent: React.FunctionComponent<DateRangePickerProps> = ({
@@ -17,75 +20,107 @@ const DateRangePickerComponent: React.FunctionComponent<DateRangePickerProps> = 
   containerHeight = 150,
   minDate = new Date(new Date().getFullYear() - 1, new Date().getMonth(), new Date().getDate()),
   maxDate = new Date(),
+  startDate,
+  endDate
 }) => {
-  const [startDate, setStartDate] = React.useState<Date | undefined>(undefined);
-  const [endDate, setEndDate] = React.useState<Date | undefined>(undefined);
 
 
+  //const [startDate, setStartDate] = React.useState<Date | undefined>(defaultStartDate);
+  //const [endDate, setEndDate] = React.useState<Date | undefined>(defaultEndDate);
+
+  const startDatePickerRef = React.useRef<IDatePicker>(null);
+  const endDatePickerRef = React.useRef<IDatePicker>(null);
+/*
   const handleStartDateChange = React.useCallback((date: Date | null | undefined) => {
+
     setStartDate(date || undefined);
-    if (date && (!endDate || date > endDate)) {
-      if (onEndDateChange) {
-        setEndDate(date);
-        onEndDateChange(date);
-      }
-    } else if (date && endDate && date < endDate) {
-      setEndDate(date);
-    } else if (date && !endDate) {
-      setEndDate(date);
-    }
     if (onStartDateChange) {
       onStartDateChange(date || undefined);
     }
-  }, [endDate, onEndDateChange, onStartDateChange]);
+    if (date && (!endDate || date > endDate)) {
+      setEndDate(date || undefined);
+      if (onEndDateChange) {
+        onEndDateChange(date || undefined);
+      }
+      //endDatePickerRef.current?.focus();
+    }
+    if (notifyFn){
+      notifyFn()
+    }
+  }, [endDate, onStartDateChange, onEndDateChange]);
 
   const handleEndDateChange = React.useCallback((date: Date | null | undefined) => {
+
     setEndDate(date || undefined);
     if (onEndDateChange) {
       onEndDateChange(date || undefined);
     }
-  }, [onEndDateChange]);
+    if (date && (!startDate || date < startDate)) {
+      setStartDate(date || undefined);
+      if (onStartDateChange) {
+        onStartDateChange(date || undefined);
+      }
+      //startDatePickerRef.current?.focus();
+    }
+    if (notifyFn){
+      notifyFn()
+    }
+  }, [startDate, onStartDateChange, onEndDateChange]);
+*/
+
+  //const datePickerStyles: Partial<IDatePickerStyles> = { root: { minWidth: 100, marginTop: 15, textAlign: 'left' }};
+
+  const onFormatDate = (date?: Date): string => {
+    return !date ? '' : (date.getMonth() + 1) + '/' + date.getDate() + '/' +  (date.getFullYear());
+  };
 
   const styles = React.useMemo(() => mergeStyleSets({
     wrapper: {
       display: 'flex',
-      justifyContent: 'space-between',
+      //justifyContent: 'space-between',
       width: '100%',
-      height: '100%',
+      height: '100%'
     },
-    datePicker: { width: containerWidth, height: containerHeight },
+    datePicker: { width: containerWidth/2-1, height: 55, minWidth: 100, marginTop: 15, textAlign: 'left'},
   }), []);
 
   return (
     <div
-      style={{
-        display: 'flex',
-        // justifyContent: 'space-between',
-        gap: '16px',
-      }}
+      className={styles.wrapper}
     >
       <DatePicker
-        style= {{width: containerWidth/2 - 8, height: containerHeight }}
+        //style= {{width: containerWidth/2 - 1, height: containerHeight, fontSize: 30 }}
+        componentRef={startDatePickerRef}
+        className={styles.datePicker}
         label="Start Date"
         placeholder="Select a start date"
         allowTextInput
         value={startDate}
-        onSelectDate={handleStartDateChange}
+        onSelectDate={onStartDateChange}
         firstDayOfWeek={DayOfWeek.Sunday}
+        isRequired={false}
         minDate={minDate}
+        maxDate={maxDate}
+        //styles={datePickerStyles}
+        formatDate={onFormatDate}
       />
 
       <DatePicker
-        style= {{width: containerWidth/2 - 8, height: containerHeight }}
+        componentRef={endDatePickerRef}
+        className={styles.datePicker}
         label="End Date"
         placeholder="Select an end date"
         allowTextInput
         value={endDate}
-        onSelectDate={handleEndDateChange}
+        onSelectDate={onEndDateChange}
         firstDayOfWeek={DayOfWeek.Sunday}
-        minDate={startDate}
+        isRequired={false}
+        minDate={minDate}
         maxDate={maxDate}
+        //styles={datePickerStyles}
+        formatDate={onFormatDate}
       />
+
     </div>
   );
 };
--- PCF/DateRangePicker/index.ts
+++ PCF/DateRangePicker/index.ts
@@ -1,39 +1,61 @@
 import { IInputs, IOutputs } from "./generated/ManifestTypes"; // Importing the input and output types from the generated ManifestTypes
 import DateRangePickerComponent from "./DateRangePickerComponent"; // Importing the DateRangePicker component
 import * as React from "react"; // Importing React
+//import { v4 as uuid } from 'uuid';
 
 export class DateRangePicker implements ComponentFramework.ReactControl<IInputs, IOutputs> {
     // Private variables
     private theComponent: ComponentFramework.ReactControl<IInputs, IOutputs>; // Reference to the ReactControl instance
     private notifyOutputChanged: () => void; // Callback to notify the framework about output changes
-    private startDate: Date | undefined; // Start date value
-    private endDate: Date | undefined; // End date value
+    private startDate: Date | null | undefined; // Start date value
+    private endDate: Date | null | undefined; // End date value
+    //private uuid: string;
+    private workaroundFlag: boolean
 
-    constructor() { }
+    constructor() {
+        //this.uuid = uuid()
+        //console.log(`#### Constructor(): ${this.uuid}`)
+        this.workaroundFlag = false
+    }
 
     public init(
         context: ComponentFramework.Context<IInputs>,
         notifyOutputChanged: () => void,
         state: ComponentFramework.Dictionary
     ): void {
+        //console.log(`### init(defStart=${context.parameters.DefaultStartDate.raw}, defEnd=${context.parameters.DefaultEndDate.raw}): ${this.uuid}`)
         context.mode.trackContainerResize(true); // Track container resize
         this.notifyOutputChanged = notifyOutputChanged;
+        this.startDate = context.parameters.DefaultStartDate.raw || new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()-1);
+        this.endDate = context.parameters.DefaultEndDate.raw || new Date();
     }
 
-    private handleStartDateChange = (date: Date | undefined): void => {
+    private handleStartDateChange = (date: Date | null | undefined): void => {
+        //console.log(`### handleStartDateChange(): ${this.uuid}`)
         this.startDate = date; // Update the start date property with the selected date
+        if (date && (!this.endDate || date > this.endDate)) {
+            this.endDate = this.startDate
+        }
+        this.workaroundFlag = true;
         this.notifyOutputChanged(); // Notify the framework about the output change
     };
 
-    private handleEndDateChange = (date: Date | undefined): void => {
+    private handleEndDateChange = (date: Date | null | undefined): void => {
+        //console.log(`### handleEndDateChange(): ${this.uuid}`)
         this.endDate = date; // Update the end date property with the selected date
+        if (date && (!this.startDate || date < this.startDate)) {
+            this.startDate = this.endDate
+        }
+        this.workaroundFlag = true;
         this.notifyOutputChanged(); // Notify the framework about the output change
     };
+
 
     public updateView(context: ComponentFramework.Context<IInputs>): React.ReactElement {
+        //console.log(`### updateView(start=${this.startDate}, end=${this.endDate}, defStart=${context.parameters.DefaultStartDate.raw}, defEnd=${context.parameters.DefaultEndDate.raw}): ${this.uuid}`)
         let width = context.mode.allocatedWidth;
         let height = context.mode.allocatedHeight;
-    
+
         // If width or height is -1, set it to a default value
         if (width === -1) {
             width = 400; // or whatever default value you want to use
@@ -41,7 +63,7 @@ export class DateRangePicker implements ComponentFramework.ReactControl<IInputs,
         if (height === -1) {
             height = 150; // or whatever default value you want to use
         }
-                
+
         return React.createElement(
             DateRangePickerComponent, // Render the DateRangePicker component
             {
@@ -50,15 +72,18 @@ export class DateRangePicker implements ComponentFramework.ReactControl<IInputs,
                 containerHeight: height, // Pass the container height
                 containerWidth: width, // Pass the container width
                 minDate: context.parameters.MinDate.raw || undefined, // Pass the minimum date value
-                maxDate: context.parameters.MaxDate.raw || undefined // Pass the maximum date value
+                maxDate: context.parameters.MaxDate.raw || undefined, // Pass the maximum date value
+                startDate: this.workaroundFlag ? this.startDate || undefined : context.parameters.DefaultStartDate.raw || undefined, // Pass the start date value
+                endDate: this.workaroundFlag ? this.endDate || undefined : context.parameters.DefaultEndDate.raw || undefined // Pass the end date value
             }
         );
     }
 
     public getOutputs(): IOutputs {
+        //console.log(`### getOutputs(): ${this.uuid}`)
         return {
-            StartDate: this.startDate, // Return the current start date value as an output
-            EndDate: this.endDate // Return the current end date value as an output
+            StartDate: this.startDate || undefined, // Return the current start date value as an output
+            EndDate: this.endDate || undefined // Return the current end date value as an output
         };
     }
 
